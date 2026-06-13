Simple-Station/Einstein-Engines

Jumpsuit toggle and resprites (#6160)
* toggelable tops clothing enable, sec section

* dont even ask... it just works

* meta fix + BSO toggle

* PDA visual code

port from https://github.com/Goob-Station/Goob-Station/pull/5848

* moved modified jumpsuits/skirts to goobmod

* fixed + temp monkey sprites

* fix rsi issue

YES I KNOW THESE ARE NOT IN PALLET

* fileshare push

* update

* pen + RD

* rsi issue fix

* cargo update

* sci inhand error fix

* valid RSI

* filepath naming fix

* joel

* outter command PDA fix

* FUck you
bad
joel
filepath naming fix
valid RSI
cargo update
rsi issue fix
pen + RD
update
fileshare push
fix rsi issue

YES I KNOW THESE ARE NOT IN PALLET
fixed + temp monkey sprites
moved modified jumpsuits/skirts to goobmod
PDA visual code

port from https://github.com/Goob-Station/Goob-Station/pull/5848
meta fix + BSO toggle
dont even ask... it just works
toggelable tops clothing enable, sec section

 Conflicts:
	Resources/Prototypes/Entities/Clothing/Uniforms/color_jumpskirts.yml
	Resources/Prototypes/Entities/Clothing/Uniforms/color_jumpsuits.yml
	Resources/Prototypes/Entities/Clothing/Uniforms/jumpskirts.yml
	Resources/Prototypes/Entities/Clothing/Uniforms/jumpsuits.yml
	Resources/Prototypes/Entities/Objects/Devices/pda.yml
	Resources/Prototypes/Loadouts/Jobs/Science/research_director.yml
	Resources/Prototypes/Loadouts/loadout_groups.yml
	Resources/Prototypes/_Goobstation/Entities/Clothing/Uniforms/jumpskirts.yml
	Resources/Prototypes/_Goobstation/Entities/Clothing/Uniforms/jumpsuits.yml
	Resources/Prototypes/_Goobstation/Loadouts/Jobs/Cargo/salvage.yml
	Resources/Prototypes/_Lavaland/Entities/Clothing/Uniforms/jumpsuits.yml
	Resources/Textures/_Goobstation/Clothing/Uniforms/Jumpskirt/Security/security_grey.rsi/meta.json
	Resources/Textures/_Goobstation/Clothing/Uniforms/Jumpsuit/Centcom/blueshield_shirt.rsi/meta.json
	Resources/Textures/_Goobstation/Clothing/Uniforms/Jumpsuit/blueshield_officer.rsi/equipped-INNERCLOTHING.png

Content.Client/PDA/PdaVisualizerSystem.cs
@@ -25,12 +25,16 @@ public sealed class PdaVisualizerSystem : VisualizerSystem<PdaVisualsComponent>
 
         if (AppearanceSystem.TryGetData<bool>(uid, PdaVisuals.IdCardInserted, out var isCardInserted, args.Component))
             _sprite.LayerSetVisible((uid, args.Sprite), PdaVisualLayers.IdLight, isCardInserted);
+        //goob addition for pen visual
+        if (AppearanceSystem.TryGetData<bool>(uid, PdaVisuals.PenInserted, out var isPenInserted, args.Component))
+            _sprite.LayerSetVisible((uid, args.Sprite), PdaVisualLayers.Pen, isPenInserted);
     }
 
     public enum PdaVisualLayers : byte
     {
         Base,
         Flashlight,
-        IdLight
+        IdLight,
+        Pen //goob addition for pen visual
     }
 }

10kee/Inky-Station

Content.Client/PDA/PdaVisualizerSystem.cs
@@ -19,12 +19,16 @@ public sealed class PdaVisualizerSystem : VisualizerSystem<PdaVisualsComponent>
 
         if (AppearanceSystem.TryGetData<bool>(uid, PdaVisuals.IdCardInserted, out var isCardInserted, args.Component))
             SpriteSystem.LayerSetVisible((uid, args.Sprite), PdaVisualLayers.IdLight, isCardInserted);
+        //goob addition for pen visual
+        if (AppearanceSystem.TryGetData<bool>(uid, PdaVisuals.PenInserted, out var isPenInserted, args.Component))
+            SpriteSystem.LayerSetVisible((uid, args.Sprite), PdaVisualLayers.Pen, isPenInserted);
     }
 
     public enum PdaVisualLayers : byte
     {
         Base,
         Flashlight,
-        IdLight
+        IdLight,
+        Pen //goob addition for pen visual
     }
 }